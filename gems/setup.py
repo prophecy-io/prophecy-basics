ebba774ab602a173753a4ab0f17e92b2feab7747
abhinavjain <abhinav@simpledatalabs.com>
2025-06-23 15:14:40 +0530

    initial commit

---
+++ gems/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+    name = 'ProphecyBasics',
+    version = '0.0.1.dev0',
+    packages = ['ProphecyBasics'],
+    package_dir = {'ProphecyBasics': '.'},
+    description = '',
+    install_requires = [],
+)
+
