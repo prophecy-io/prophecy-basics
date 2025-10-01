336a4e04714131edce77025fa473fe00aedf3a63
abhinavjain <abhinav@simpledatalabs.com>
2025-10-01 18:39:05 +0530

    version bump up

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prophecy_basics",
-    version="0.0.1.dev1",
+    version="0.0.1.dev2",
     packages=["prophecy_basics"],
     package_dir={"prophecy_basics": "."},
     description="",
