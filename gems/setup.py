3805743f228a833b21876e3ad0a5e50cf4fa7918
abhinavjain <abhinav@simpledatalabs.com>
2025-10-21 12:21:13 +0530

    dev tag 0.0.1.dev6

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prophecy_basics",
-    version="0.0.1.dev5",
+    version="0.0.1.dev6",
     packages=["prophecy_basics"],
     package_dir={"prophecy_basics": "."},
     description="",
