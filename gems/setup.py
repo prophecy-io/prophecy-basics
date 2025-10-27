fe0433c8ef80c93385788996d68f019aee6110c4
abhinavjain <abhinav@simpledatalabs.com>
2025-10-27 18:27:20 +0530

    generate_rows spaces handling

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prophecy_basics",
-    version="1.0.1.dev0",
+    version="1.0.1.dev1",
     packages=["prophecy_basics"],
     package_dir={"prophecy_basics": "."},
     description="",
