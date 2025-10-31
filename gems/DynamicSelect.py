1fe789f53ebd4b5940550dc88e5425d1086c1f2c
Hayden Blair <blair.hayden@prophecy.io>
2025-10-30 17:08:50 -0700

    adding applyPython for dynamic select

--- gems/DynamicSelect.py
+++ gems/DynamicSelect.py
@@ -329,3 +329,31 @@ class DynamicSelect(MacroSpec):
             relation_name=relation_name,
         )
         return component.bindProperties(newProperties)
+
+    def applyPython(self, spark: SparkSession, in0: DataFrame, props: DynamicSelectProperties) -> DataFrame:
+            if props.selectUsing == "SELECT_FIELD_TYPES":
+                desired_types = []
+                type_mapping = {
+                    'strTypeChecked': "string",
+                    'intTypeChecked': "int",
+                    'boolTypeChecked': "boolean",
+                    'shortTypeChecked': "short",
+                    'byteTypeChecked': "byte",
+                    'longTypeChecked': "long",
+                    'floatTypeChecked': "float",
+                    'doubleTypeChecked': "double",
+                    'decimalTypeChecked': "decimal",
+                    'binaryTypeChecked': "binary",
+                    'dateTypeChecked': "date",
+                    'timestampTypeChecked': "timestamp",
+                    'structTypeChecked': "struct"
+                }
+                for prop, type_name in type_mapping.items():
+                    if getattr(props, prop):
+                        desired_types.append(type_name)
+
+                from prophecy.libs.utils import filter_columns_by_type
+                return filter_columns_by_type(spark, in0, ",".join(desired_types))
+            else:
+                from prophecy.libs.utils import filter_columns_by_expr
+                return filter_columns_by_expr(spark, in0, props.customExpression)
