71c5096b6be76949d7a52e50114834d92a48fadc
Debashis Adak <debashisadak@Debashiss-MacBook-Pro.local>
2025-11-04 17:28:11 +0530

    JSONParse - Changes

--- gems/JSONParse.py
+++ gems/JSONParse.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from prophecy.cb.sql.Component import *
 from prophecy.cb.sql.MacroBuilderBase import *
 from prophecy.cb.ui.uispec import *
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import *
 
 
 class JSONParse(MacroSpec):
@@ -280,3 +282,29 @@ class JSONParse(MacroSpec):
             component,
             properties=replace(component.properties, relation_name=relation_name),
         )
+
+    def applyPython(self, spark: SparkSession, in0: DataFrame) -> DataFrame:        
+        if self.props.parsingMethod == "parseFromSchema":
+            if not self.props.sampleSchema or self.props.sampleSchema.strip() == "":
+                return in0
+            schema_str = self.props.sampleSchema.replace("\n", " ").strip()
+            quoted_col = f"`{self.props.columnName}`"
+            alias_col = f"`{self.props.columnName}_parsed`"
+            return in0.selectExpr(
+                "*",
+                f"from_json({quoted_col}, '{schema_str}') as {alias_col}"
+            )
+            
+        elif self.props.parsingMethod == "parseFromSampleRecord":
+            if not self.props.sampleRecord or self.props.sampleRecord.strip() == "":
+                return in0
+            sample_str = self.props.sampleRecord.replace("\n", " ").strip()
+            sample_str_escaped = sample_str.replace("'", "\\'")
+            quoted_col = f"`{self.props.columnName}`"
+            alias_col = f"`{self.props.columnName}_parsed`"
+            return in0.selectExpr(
+                "*",
+                f"from_json({quoted_col}, schema_of_json('{sample_str_escaped}')) as {alias_col}"
+            )
+        else:
+            return in0
