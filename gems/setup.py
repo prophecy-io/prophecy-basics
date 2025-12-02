30624b61f6ee90c6befa29934f487efa03ed810d
Debashis Adak <debashisadak@Debashiss-MacBook-Pro.local>
2025-12-02 19:21:02 +0530

    1212249917049863 - Parse JSON Issue Fix

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prophecy_basics",
-    version="1.0.4.dev6",
+    version="1.0.4.dev7",
     packages=["prophecy_basics"],
     package_dir={"prophecy_basics": "."},
     description="",
