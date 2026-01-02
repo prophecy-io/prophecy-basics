01f99ca68fa4b470c65b6d9d81925a8770b8e692
nadeeshgarg <nadeesh@prophecy.io>
2026-01-02 19:12:20 +0530

    [FIX] Putting replace_column_safe in identifier_helpers

--- macros/GenerateRows.sql
+++ macros/GenerateRows.sql
@@ -14,34 +14,6 @@
     force_mode)) }}
 {% endmacro %}
 
-{# Helper: Replace column name only when its a complete identifier (not a substring) #}
-{% macro prophecy_basics.replace_column_safe(text, plain_col, replacement) %}
-    {% set word_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_' %}
-    {% set col_len = plain_col | length %}
-    {% set result = '' %}
-    {% set i = 0 %}
-    {% while i < text | length %}
-        {% set remaining = text[i:] %}
-        {% set pos = remaining.find(plain_col) %}
-        {% if pos >= 0 %}
-            {% set actual_pos = i + pos %}
-            {% set after_pos = actual_pos + col_len %}
-            {% set after_char = '' if after_pos >= text | length else text[after_pos] %}
-            {% if after_char not in word_chars %}
-                {% set result = result ~ text[i:actual_pos] ~ replacement %}
-                {% set i = after_pos %}
-            {% else %}
-                {% set result = result ~ text[i:after_pos] %}
-                {% set i = after_pos %}
-            {% endif %}
-        {% else %}
-            {% set result = result ~ text[i:] %}
-            {% set i = text | length %}
-        {% endif %}
-    {% endwhile %}
-    {{ return(result) }}
-{% endmacro %}
-
 {% macro default__GenerateRows(
     relation_name=None,
     init_expr='1',
