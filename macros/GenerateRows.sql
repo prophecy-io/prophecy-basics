dba73053d2d0252da32a5af5b0dfc764d7977561
nadeeshgarg <nadeesh@prophecy.io>
2026-01-02 19:05:24 +0530

    [FIX]

--- macros/GenerateRows.sql
+++ macros/GenerateRows.sql
@@ -14,13 +14,31 @@
     force_mode)) }}
 {% endmacro %}
 
-{# Helper: Replace column name only when followed by space, '(', '.', or ')' (not a substring) #}
-{% macro replace_column_safe(text, plain_col, replacement) %}
-    {% set result = text %}
-    {% set result = result | replace(plain_col ~ ' ', replacement ~ ' ') %}
-    {% set result = result | replace(plain_col ~ '(', replacement ~ '(') %}
-    {% set result = result | replace(plain_col ~ '.', replacement ~ '.') %}
-    {% set result = result | replace(plain_col ~ ')', replacement ~ ')') %}
+{# Helper: Replace column name only when its a complete identifier (not a substring) #}
+{% macro prophecy_basics.replace_column_safe(text, plain_col, replacement) %}
+    {% set word_chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_' %}
+    {% set col_len = plain_col | length %}
+    {% set result = '' %}
+    {% set i = 0 %}
+    {% while i < text | length %}
+        {% set remaining = text[i:] %}
+        {% set pos = remaining.find(plain_col) %}
+        {% if pos >= 0 %}
+            {% set actual_pos = i + pos %}
+            {% set after_pos = actual_pos + col_len %}
+            {% set after_char = '' if after_pos >= text | length else text[after_pos] %}
+            {% if after_char not in word_chars %}
+                {% set result = result ~ text[i:actual_pos] ~ replacement %}
+                {% set i = after_pos %}
+            {% else %}
+                {% set result = result ~ text[i:after_pos] %}
+                {% set i = after_pos %}
+            {% endif %}
+        {% else %}
+            {% set result = result ~ text[i:] %}
+            {% set i = text | length %}
+        {% endif %}
+    {% endwhile %}
     {{ return(result) }}
 {% endmacro %}
 
@@ -80,7 +98,7 @@
     {% set _cond_tmp = _cond_tmp | replace(backtick_col, internal_col) %}
     {% set _cond_tmp = _cond_tmp | replace(doubleq_col, internal_col) %}
     {% set _cond_tmp = _cond_tmp | replace(singleq_col, internal_col) %}
-    {% set _cond_tmp = replace_column_safe(_cond_tmp, plain_col, internal_col) %}
+    {% set _cond_tmp = prophecy_basics.replace_column_safe(_cond_tmp, plain_col, internal_col) %}
     {# Restore payload.YMD - payload references should not be replaced #}
     {% set _cond_tmp = _cond_tmp | replace('payload.' ~ internal_col, 'payload.' ~ plain_col) %}
     {% set condition_expr_sql = _cond_tmp %}
@@ -91,7 +109,7 @@
     {% set _loop_tmp = _loop_tmp | replace(backtick_col, 'gen.' ~ internal_col) %}
     {% set _loop_tmp = _loop_tmp | replace(doubleq_col, 'gen.' ~ internal_col) %}
     {% set _loop_tmp = _loop_tmp | replace(singleq_col, 'gen.' ~ internal_col) %}
-    {% set _loop_tmp = replace_column_safe(_loop_tmp, plain_col, 'gen.' ~ internal_col) %}
+    {% set _loop_tmp = prophecy_basics.replace_column_safe(_loop_tmp, plain_col, 'gen.' ~ internal_col) %}
     {% set loop_expr_replaced = _loop_tmp %}
 
     {# Use adapter-safe quoting for EXCEPT column #}
@@ -299,7 +317,7 @@
     {% set _cond_tmp = _cond_tmp | replace(backtick_col, internal_col) %}
     {% set _cond_tmp = _cond_tmp | replace(doubleq_col, internal_col) %}
     {% set _cond_tmp = _cond_tmp | replace(singleq_col, internal_col) %}
-    {% set _cond_tmp = replace_column_safe(_cond_tmp, plain_col, internal_col) %}
+    {% set _cond_tmp = prophecy_basics.replace_column_safe(_cond_tmp, plain_col, internal_col) %}
     {# Restore payload.YMD - payload references should not be replaced #}
     {% set _cond_tmp = _cond_tmp | replace('payload.' ~ internal_col, 'payload.' ~ plain_col) %}
     {% set condition_expr_sql = _cond_tmp %}
@@ -310,7 +328,7 @@
     {% set _loop_tmp = _loop_tmp | replace(backtick_col, 'gen.' ~ internal_col) %}
     {% set _loop_tmp = _loop_tmp | replace(doubleq_col, 'gen.' ~ internal_col) %}
     {% set _loop_tmp = _loop_tmp | replace(singleq_col, 'gen.' ~ internal_col) %}
-    {% set _loop_tmp = replace_column_safe(_loop_tmp, plain_col, 'gen.' ~ internal_col) %}
+    {% set _loop_tmp = prophecy_basics.replace_column_safe(_loop_tmp, plain_col, 'gen.' ~ internal_col) %}
     {% set loop_expr_replaced = _loop_tmp %}
 
     {# Use adapter-safe quoting for EXCLUDE column #}
