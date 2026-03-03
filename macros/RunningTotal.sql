bc7977075a668fced4510a50c61e2480a9093f29
nadeeshgarg <nadeesh@prophecy.io>
2026-03-03 13:04:21 +0530

    Refactor RunningTotal macro by removing orderByColumns parameter and related logic. Update SQL macro to simplify windowing logic. Adjust Python implementation accordingly.

--- macros/RunningTotal.sql
+++ macros/RunningTotal.sql
@@ -1,13 +1,11 @@
 {% macro RunningTotal(relation_name,
         groupByColumnNames,
         runningTotalColumnNames,
-        outputPrefix,
-        orderByColumns= []) -%}
+        outputPrefix) -%}
     {{ return(adapter.dispatch('RunningTotal', 'prophecy_basics')(relation_name,
         groupByColumnNames,
         runningTotalColumnNames,
-        outputPrefix,
-        orderByColumns)) }}
+        outputPrefix)) }}
 {% endmacro %}
 
 
@@ -15,27 +13,10 @@
         relation_name,
         groupByColumnNames,
         runningTotalColumnNames,
-        outputPrefix,
-        orderByColumns= []
+        outputPrefix
 ) -%}
 
 {% set relation_list = relation_name if relation_name is iterable and relation_name is not string else [relation_name] %}
-{%- set order_parts = [] -%}
-{%- for r in orderByColumns %}
-  {% if r.expression.expression | trim != '' %}
-    {% set part %}
-      {{ r.expression.expression }}
-      {% if   r.sortType == 'asc'               %} asc
-      {% elif r.sortType == 'asc_nulls_last'    %} asc nulls last
-      {% elif r.sortType == 'desc_nulls_first'  %} desc nulls first
-      {% else                               %} desc
-      {% endif %}
-    {% endset %}
-    {%- do order_parts.append(part | trim) -%}
-  {% endif %}
-{%- endfor %}
-{%- set order_by_clause = order_parts | join(', ') -%}
-{%- set has_order = order_by_clause | length > 0 -%}
 {%- set has_group = groupByColumnNames | length > 0 -%}
 
 {%- set quoted_group_columns = [] -%}
@@ -43,17 +24,18 @@
     {%- do quoted_group_columns.append(prophecy_basics.quote_identifier(column)) -%}
 {%- endfor -%}
 
+{%- if has_group -%}
+    {%- set window_over = "partition by " ~ (quoted_group_columns | join(', ')) ~ " order by 1" -%}
+{%- else -%}
+    {%- set window_over = "order by 1" -%}
+{%- endif -%}
+
 {%- set run_tot_select_parts = [] -%}
 {%- for column in runningTotalColumnNames -%}
     {%- set quoted_col = prophecy_basics.quote_identifier(column) -%}
     {%- set out_col = prophecy_basics.quote_identifier(outputPrefix ~ column) -%}
-    {%- set window_expr %}
-        sum({{ quoted_col }}) over (
-            {% if has_group %}partition by {{ quoted_group_columns | join(', ') }} {% endif %}
-            {% if has_order %}order by {{ order_by_clause }}{% else %}order by 1{% endif %}
-        )
-    {% endset %}
-    {%- do run_tot_select_parts.append((window_expr | trim) ~ " as " ~ out_col) -%}
+    {%- set expr = "sum(" ~ quoted_col ~ ") over (" ~ window_over ~ ") as " ~ out_col -%}
+    {%- do run_tot_select_parts.append(expr) -%}
 {%- endfor -%}
 
 select
