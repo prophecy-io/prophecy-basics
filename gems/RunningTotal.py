e979652e2a1d6965c9f2e0bd029cb5bff66013f0
nadeeshgarg <nadeesh@prophecy.io>
2026-03-12 13:11:41 +0530

    [FIX] Running total: removing Snowflake

--- gems/RunningTotal.py
+++ gems/RunningTotal.py
@@ -34,7 +34,7 @@ class RunningTotal(MacroSpec):
     minNumOfInputPorts: int = 1
     supportedProviderTypes: list[ProviderTypeEnum] = [
         ProviderTypeEnum.Databricks,
-        ProviderTypeEnum.Snowflake,
+        # ProviderTypeEnum.Snowflake, -> Need to get Expression Table expressions' quoting sorted.
         ProviderTypeEnum.BigQuery,
         ProviderTypeEnum.ProphecyManaged,
     ]
