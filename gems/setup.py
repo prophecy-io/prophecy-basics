284ad018ea58cd90193baab6d82630c0b81f3e88
abhinavjain <abhinav@simpledatalabs.com>
2026-03-26 19:40:37 +0530

    union by name bug fix for snowflake

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prophecy_basics",
-    version="1.0.9",
+    version="1.0.10",
     packages=["prophecy_basics"],
     package_dir={"prophecy_basics": "."},
     description="",
