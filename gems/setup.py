7d3928126d226a3a64edc7e1279a02615f14a1a0
jainabhinav <abhinav@simpledatalabs.com>
2026-06-17 11:54:09 +0530

    prod tag 1.0.14

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="prophecy_basics",
-    version="1.0.14.dev3",
+    version="1.0.14",
     packages=["prophecy_basics"],
     package_dir={"prophecy_basics": "."},
     description="",
