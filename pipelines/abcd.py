9b22085a919527bf1dadfc3be42422cec0edbceb
abhishekcms611@gmail.com <abhishekcms611@gmail.com>
2026-07-17 09:22:19 +0000

    abcd

---
+++ pipelines/abcd.py
@@ -0,0 +1,11 @@
+from prophecy_pipeline_sdk.graph import *
+from prophecy_pipeline_sdk.properties import *
+args = PipelineArgs(label = "abcd", version = 1, auto_layout = False)
+
+with Pipeline(args) as pipeline:
+    abcd__testtrainsplit_1 = Process(
+        name = "abcd__TestTrainSplit_1",
+        properties = ModelTransform(modelName = "abcd__TestTrainSplit_1"),
+        input_ports = None
+    )
+
