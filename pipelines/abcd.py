8bb97c5a62c0258a935cbf79ca39944dd61a3af2
abhishekcms611@gmail.com <abhishekcms611@gmail.com>
2026-07-21 16:52:48 +0000

    Modified     - Pipeline        - abcd

--- pipelines/abcd.py
+++ pipelines/abcd.py
@@ -3,9 +3,5 @@ from prophecy_pipeline_sdk.properties import *
 args = PipelineArgs(label = "abcd", version = 1, auto_layout = False)
 
 with Pipeline(args) as pipeline:
-    abcd__testtrainsplit_1 = Process(
-        name = "abcd__TestTrainSplit_1",
-        properties = ModelTransform(modelName = "abcd__TestTrainSplit_1"),
-        output_ports = 2
-    )
+    abcd__pivot_1 = Process(name = "abcd__Pivot_1", properties = ModelTransform(modelName = "abcd__Pivot_1"))
 
